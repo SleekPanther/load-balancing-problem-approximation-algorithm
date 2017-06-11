29b826247eb634b603d9a421790979177f90e2bb
SleekPanther <macrobyte2@gmail.com>
2017-06-11 17:02:10 -0400

    initial algorithm attempt (works)

--- LoadBalancing.java
+++ LoadBalancing.java
@@ -1,10 +1,35 @@
 import java.util.*;
 
 public class LoadBalancing {
-	
-	
 	public void balanceMachines(int machineCount, int[][] jobs){
-		System.out.println(1);
+		Arrays.sort(jobs, (a, b) -> -Integer.compare(a[1], b[1]));	//Sort by  longest procession time 1st
+
+		ArrayList<ArrayList<Integer>> jobsForMachine = new ArrayList<ArrayList<Integer>>();
+		PriorityQueue<Machine> loads = new PriorityQueue<Machine>();
+		for(int i=0; i<machineCount; i++){
+			loads.add(new Machine(i));
+			jobsForMachine.add(new ArrayList<Integer>());
+		}
+		
+		for(int j=0; j<jobs.length; j++){
+			Machine smallestLoadMachine = loads.remove();
+			int i=smallestLoadMachine.getId();
+			jobsForMachine.get(i).add(jobs[j][0]);
+			smallestLoadMachine.setCurrentLoad(smallestLoadMachine.getCurrentLoad() + jobs[j][1]);
+			loads.add(smallestLoadMachine);
+		}
+		
+		System.out.println("Jobs Assignments:");
+		for(int i=0;  !loads.isEmpty();  i++){
+			Machine machine = loads.remove();
+			System.out.print("Machine "+machine.getId()+": ");
+			ArrayList<Integer> assignedJobs = jobsForMachine.get(i);
+			for(int k=0; k<assignedJobs.size(); k++){
+				System.out.print("\nJob "+assignedJobs.get(k)+": (Processing time="+jobs[k][1]+")");
+			}
+			System.out.println("\n");
+		}
+		
 	}
 
 	class Machine implements Comparable<Machine>{
@@ -41,6 +66,7 @@ public class LoadBalancing {
 
 	public static void main(String[] args) {
 		LoadBalancing loadBalancer = new LoadBalancing();
+		int machineCount1 = 4;
 		int[][] jobs1 = {
 						{1, 2},
 						{2, 2},
@@ -50,11 +76,7 @@ public class LoadBalancing {
 						{6, 1},
 						{7, 4}
 						};
-		loadBalancer.balanceMachines(4, jobs1);
-		
-		ArrayList arrlist = new ArrayList();
-		Collections.addAll(arrlist, "1","2","3");
-		System.out.println("Final collection value: "+arrlist);
+		loadBalancer.balanceMachines(machineCount1, jobs1);
 	}
 
 }
